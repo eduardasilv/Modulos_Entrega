124759a57900f6dc87e2e70c90ebd2fb7aed91e3
Guigaspar26 <73366398+Guigaspar26@users.noreply.github.com>
2020-12-07 23:28:47 +0000

    Create modGrafos.py

---
+++ modGrafos.py
@@ -0,0 +1,18 @@
+##GRAFOS
+
+def nos(g):
+    return g[0]
+
+def arestas(g):
+    return g[1]
+
+def copias(g,k): #cada elemento de "copias" é um individuo inicial
+    r=[]
+    for x in arestas(g):
+        r+=[x]*k
+    for i in range(len(r)):
+        r[i]= [r[i],[], i+1]
+    return r
+
+def numnos(g):
+    return len(nos(g))
