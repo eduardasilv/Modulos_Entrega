5c691e2201728eed9ec63a829eb2347342fb5b62
Guigaspar26 <73366398+Guigaspar26@users.noreply.github.com>
2020-12-07 23:34:29 +0000

    Update modIndividuos.py

--- modIndividuos.py
+++ modIndividuos.py
@@ -24,27 +24,4 @@ def first(ind):
 def last(ind):
     return caminho(ind)[-1]
 
-def od(ind,o,d):
-    if first(ind)!=o or last(ind)!=d:
-        return False
-    else:
-        return True
-    
-def length(ind,g):
-    if size(ind)!=numnos(g):
-        return False
-    else:
-        return True
-    
-def todos(ind,g):
-    ok=True 
-    for x in nos(g):
-        if x not in caminho(ind):
-            ok=False
-    return ok
 
-def hamilton(ind,g,o,d):
-    if od(ind,o,d) and length(ind,g) and todos(ind,g):
-        return True
-    else:
-        return False
