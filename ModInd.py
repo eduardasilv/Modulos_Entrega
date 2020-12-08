973c58d8a2e72f894d891b777ada6d03d32fa404
Maria Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2020-12-08 13:23:08 +0000

    Update and rename modIndividuos.py to ModInd.py
    tem funções novas: novocaminho, novapos, novoID

---
+++ ModInd.py
@@ -0,0 +1,42 @@
+### INDIVIDUOS
+#    individuo = [caminho, pos]
+#    pos=[x,y,z]
+#    caminho=[lista de nos por onde passa]
+#    comprimento de individuo = len(individuo[0])
+
+def ind(caminho, pos,ID):
+    return [caminho, pos, ID]
+
+def caminho(ind):
+    return ind[0] # = len(caminho(ind))
+
+def posicao(ind):
+    return ind[1]
+
+def ID(ind):
+    return ind[2]
+
+def size(ind):
+    return len(ind[0])
+
+def novoind():
+    return[[],[],[]]
+
+def novocaminho(ind,caminho):
+    ind[0]=caminho
+    return ind
+
+def novapos(ind,pos):
+    ind[1]=pos
+    return ind
+
+def novoID(ind,ID):
+    ind[2]=ID
+    return ind
+
+def first(ind):
+    return caminho(ind)[0]
+
+def last(ind):
+    return caminho(ind)[-1]
+
