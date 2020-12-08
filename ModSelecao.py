74b83bd91adb63ef4ceffaf16f6109bab7e254e4
Maria Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2020-12-08 13:23:55 +0000

    Update and rename modSelecao.py to ModSelecao.py

--- ModSelecao.py
+++ ModSelecao.py
@@ -1,30 +1,35 @@
+#SELEÇÃO
+import ModGrafos as gr
+import ModInd as i
+
 def od(ind,o,d):
-    if first(ind)!=o or last(ind)!=d:
+    if i.first(ind)!=o or i.last(ind)!=d:
         return False
     else:
         return True
     
 def length(ind,g):
-    if size(ind)!=numnos(g):
+    if i.size(ind)!=gr.numnos(g):
         return False
     else:
         return True
     
 def todos(ind,g):
     ok=True 
-    for x in nos(g):
-        if x not in caminho(ind):
+    for x in gr.nos(g):
+        if x not in i.caminho(ind):
             ok=False
-    return ok
+    return ok   
 
 def hamilton(ind,g,o,d):
     if od(ind,o,d) and length(ind,g) and todos(ind,g):
         return True
     else:
         return False
+    
 def final(sopa,g,o,d):
     r=[]
     for individuo in sopa:
         if hamilton(individuo,g,o,d):
-            r+=[[caminho(individuo)]]
+            r+=[[i.caminho(individuo)]]
     return r
