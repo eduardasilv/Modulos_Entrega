fc81a90eef1b97f25f480a4c1636317748501649
eduardasilv <75646684+eduardasilv@users.noreply.github.com>
2020-12-10 00:07:16 +0000

    Update projeto2.py

--- projeto2.py
+++ projeto2.py
@@ -2,53 +2,54 @@ import random
 import ModInd as i
 import ModSopa as sp
 import ModEvent as ev
-import ModGrafo as gr
+import ModGrafos as gr
+import ModSelecao as sel
+import ModCap as cap
 
 def sim(g,o,d,k,limite,tbc,tbd,tbz):
  
-    sopa = posInicial(g,k)
+    sopa = sp.posInicial(g,k)
  
     c=[]
-    for x in conjIDS(sopa):
-        c = addE(c,[event(exprandom(tbc),"con",x)])
-        c = addE(c,[event(exprandom(tbd),"des",x)])
-    c = addE(c,[event(exprandom(tbz),"cis",0)])
+    for x in sp.conjIDS(sopa):
+        c = cap.addE(c,[ev.event(exprandom(tbc),"con",x)])
+        c = cap.addE(c,[ev.event(exprandom(tbd),"des",x)])
+    c = cap.addE(c,[ev.event(exprandom(tbz),"cis",0)])
 
-    ce = nextE(c)    #current event
-    ct = time(ce)  #current time = agora
-    ck = kind(ce)  #current kind
-    cid = IDe(ce)  #current IDe
+    ce = cap.nextE(c)    #current event
+    ct = ev.time(ce)  #current time = agora
+    ck = ev.kind(ce)  #current kind
+    cid = ev.IDe(ce)  #current IDe
 
     while ct <= limite:
 
         if ck == "con":
             for individuo in sopa:
-                if ID(individuo)==cid:
+                if i.ID(individuo)==cid:
                     ind1=individuo
-            #ind 2 é um aleatorio dos "cincoprox (ind1,sopa)" 
             ind2=random.choice(sp.cincoprox(ind1,sopa))
     
             "Modulo concatenacao"
             #ind3 = con(ind1,ind2,sopa)
-            sopa = removeS(ind1,sopa)
-            sopa = removeS(ind2,sopa)
-            sopa = addS(ind3,sopa)
-            c = addE(c,event(ct+exprandom(tbc),"con",ID(ind3)))
-            c = addE(c,event(ct+exprandom(tbd),"des",ID(ind3)))    # tbd???
+            sopa = sp.removeS(ind1,sopa)
+            sopa = sp.removeS(ind2,sopa)
+            sopa = sp.addS(ind3,sopa)
+            c = cap.addE(c,ev.event(ct+exprandom(tbc),"con",i.ID(ind3)))
+            c = cap.addE(c,ev.event(ct+exprandom(tbd),"des",i.ID(ind3)))
             # eliminar da cap os eventos previstos 
             for evt in c:
-                if IDe(evt)==ID(ind1) or IDe(evt)==ID(ind2):
-                    c = removeE(c,evt)
+                if ev.IDe(evt)==i.ID(ind1) or ev.IDe(evt)==i.ID(ind2):
+                    c = cap.removeE(c,evt)
             
         elif ck == "des":
             "Modulo deslocamento"
-            c=addE(c,event(ct+random(tbc),"des",cid))
+            c= cap.addE(c,ev.event(ct+random(tbc),"des",cid))
         else:
             "Modulo cisão"
-            c=addE(c,event(ct+random(tbz),"cis",0))
+            c= cap.addE(c,ev.event(ct+random(tbz),"cis",0))
 
-        ce = nextE(c)
-        ct = time(ce)
-        ck = kind(ce)
-        cid = IDe(ce)
-        c=delE(c)
+        ce = cap.nextE(c)
+        ct = ev.time(ce)
+        ck = ev.kind(ce)
+        cid = ev.IDe(ce)
+        c = cap.delE(c)
