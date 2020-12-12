f9738f85ecf1226e90cabbcd1368818865a00ce7
Maria Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2020-12-12 17:46:29 -0100

    Update ModCap.py

--- ModCap.py
+++ ModCap.py
@@ -1,21 +1,4 @@
 ## CAP
-def event(t,k,IDe):
-    if k=="con" or k=="des" or k=="cis":
-        return [t,k,IDe]
-    else:
-        return ("Erro do tipo de evento!")
-
-def kind(e):
-    if e[1]=="con" or e[1]=="des" or e[1]=="cis":
-        return e[1]
-    else:
-        return ("Erro do tipo de evento!")
-
-def time(e):
-    return e[0]
-
-def IDe(e):
-    return e[2]
 
 def addE(c,e):
     return [x for x in c if time(x)<time(e)] + [e] + [x for x in c if time(x)>time(e)]
