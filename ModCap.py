e5349f8fd1d4bc2facc1d4187dbdbe4415b79513
eduardasilv <75646684+eduardasilv@users.noreply.github.com>
2020-12-12 13:03:00 +0000

    Update ModCap.py

--- ModCap.py
+++ ModCap.py
@@ -2,10 +2,14 @@
 def event(t,k,IDe):
     if k=="con" or k=="des" or k=="cis":
         return [t,k,IDe]
+    else:
+        return ("Erro do tipo de evento!")
 
 def kind(e):
     if e[1]=="con" or e[1]=="des" or e[1]=="cis":
         return e[1]
+    else:
+        return ("Erro do tipo de evento!")
 
 def time(e):
     return e[0]
