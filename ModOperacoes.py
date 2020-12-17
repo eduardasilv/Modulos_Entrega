1ba05b296e202703afe40b8b46a7392187ea660b
Maria Clara Menezes <62575360+mariaclaramenezes2001@users.noreply.github.com>
2020-12-17 17:00:32 -0100

    Update ModOperacoes.py

--- ModOperacoes.py
+++ ModOperacoes.py
@@ -104,10 +104,10 @@ def cisao(sopa,g):
         for individuo in sopa:    
                 
             if i.size(individuo) > gr.numnos(g):
-                s=int(random.uniform(1,(gr.numnos(g)/2)-1)) #como adicionamos um no , tem de se tirar aqui
+                s=int(random.uniform(1,(gr.numnos(g)/2))) #como adicionamos um no , tem de se tirar aqui
                 
                 while i.size(individuo)-s < 2:    
-                    s=int(random.uniform(1,(gr.numnos(g)/2)-1))   #novo len do caminho que é escolhido aleatoriamente entre 2 e metade do desejado, com -1 porque adicionamos a seguir
+                    s=int(random.uniform(1,(gr.numnos(g)/2)))   #novo len do caminho que é escolhido aleatoriamente entre 2 e metade do desejado, com 1 porque adicionamos a seguir
                       
                 ind1=i.novoind()
                 ind1=i.novocaminho(ind1,i.caminho(individuo)[:s+1])
